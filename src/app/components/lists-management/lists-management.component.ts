b8b680826a9c113d2da0f93c928a4def78914735
minhthuan274 <doanminhthuan274@gmail.com>
2017-06-20 10:06:22 +0700

    add column shares

--- src/app/components/lists-management/lists-management.component.ts
+++ src/app/components/lists-management/lists-management.component.ts
@@ -7,6 +7,7 @@ import { environment } from '../../../environments/environment';
 class List { 
   title: string;
   user_email: string;
+  shares: string;
   todo_count: number;
   done_count: number;
 }
